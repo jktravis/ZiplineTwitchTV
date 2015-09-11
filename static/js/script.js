4b1dc864f0bbd47ba5f59012b0c0e7a5bc20a67c
jktravis <joshua.travis@gmail.com>
2015-09-11 08:20:48 -0400

    Now pulls in channel's images.

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,36 @@
+$(document).ready(function()
+{
+
 var twitchUsers =
     ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff", "medrybw"];
 
-var baseUrl = 'https://api.twitch.tv/kraken/streams/';
+var streamUrl = 'https://api.twitch.tv/kraken/streams/';
+var channelUrl = 'https://api.twitch.tv/kraken/channels/';
+var $statusList = $('#status-list');
+
+$statusList.empty();
 
 for (var i = 0; i < twitchUsers.length; i++)
 {
-    $.getJSON(baseUrl + twitchUsers[i], function(resp)
+    $.getJSON(channelUrl + twitchUsers[i], function(resp)
     {
-        if (resp.stream === null)
-        {
-            console.log(resp._links.channel + ' is offline');
-        }
-        else
-        {
-            console.log('Playing ' + resp.stream.game);
-            console.log(resp.stream.channel.name + " is online");
-        }
+        $statusList.append('<li><a href="' + resp.url + '"><img src="' + resp.logo + '" class="img-thumbnail img-circle"/> ' + resp.display_name + '</a></li>');
     })
 }
+
+//for (i = 0; i < twitchUsers.length; i++)
+//{
+//    $.getJSON(streamUrl + twitchUsers[i], function(resp)
+//    {
+//        if (resp.stream === null)
+//        {
+//            console.log(resp._links.channel + ' is offline');
+//        }
+//        else
+//        {
+//            console.log('Playing ' + resp.stream.game);
+//            console.log(resp.stream.channel.name + " is online");
+//        }
+//    })
+//}
+});
