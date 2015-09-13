3e10697148e0858335f551f67c48b3aacbc80118
jktravis <joshua.travis@gmail.com>
2015-09-13 13:09:35 -0400

    UI is starting to look good.

--- static/js/script.js
+++ static/js/script.js
@@ -7,11 +7,14 @@ $(document).ready(function()
     var streamUrl = 'https://api.twitch.tv/kraken/streams/';
     var channelUrl = 'https://api.twitch.tv/kraken/channels/';
     var $statusList = $('#status-list');
-    var listTemplate = '<li><a href="$URL"><img src="$IMG" class="img-thumbnail img-circle"/> $DISPLAY_NAME' +
-        '<span class="glyphicon glyphicon-$STATUS">$PLAYING</span></a>' +
-        '</li>';
+    //var listTemplate = '<li><a href="$URL"><img src="$IMG" class="img-thumbnail img-circle"/> $DISPLAY_NAME' +
+    //    '<span class="glyphicon glyphicon-$STATUS text-right"></span>$PLAYING</a></li>';
+
+    var listTemplate = '<div class="row list-item"> <div class="col-lg-2"><img src="$IMG" class="img-thumbnail img-circle"/></div>' +
+        '<div class="col-lg-8"> <a href="$URL">$DISPLAY_NAME</a> </div> <div class="col-lg-1"> <span class="glyphicon glyphicon-$STATUS"></span> </div>' +
+        '<div class="col-lg-10 col-lg-offset-2 description"> $PLAYING </div> </div>';
     var currentListItem;
-    $statusList.empty();
+    //$statusList.empty();
 
     for (var i = 0; i < twitchUsers.length; i++)
     {
@@ -20,7 +23,8 @@ $(document).ready(function()
             console.log(user[0].name);
             console.log(stream[0]);
             user[0].logo = user[0].logo === null ? 'http://lorempixel.com/60/60/' : user[0].logo;
-            currentListItem = listTemplate.replace('$URL', user[0].url).replace('$IMG', user[0].logo).replace('$DISPLAY_NAME', user[0].display_name);
+            currentListItem = listTemplate.replace('$URL', user[0].url).replace('$IMG', user[0].logo)
+                .replace('$DISPLAY_NAME', user[0].display_name);
             if (stream[0].stream === null)
             {
                 currentListItem = currentListItem.replace('$STATUS', 'pause').replace('$PLAYING', 'Offline');
@@ -30,6 +34,9 @@ $(document).ready(function()
                 currentListItem = currentListItem.replace('$STATUS', 'play').replace('$PLAYING', stream[0].stream.game);
             }
             $statusList.append(currentListItem);
+        }).fail(function()
+        {
+            $statusList.append('<li>Unable to load data</li>');
         });
     }
 });
