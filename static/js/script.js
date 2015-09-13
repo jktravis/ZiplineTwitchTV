8f6ea5d2f638cefbccd5ed1e4a51855bb18567be
jktravis <joshua.travis@gmail.com>
2015-09-13 11:42:24 -0400

    Using jQuery's $.when() method, I am now able to appropriately work with each object when all the data is available. Yay!

--- static/js/script.js
+++ static/js/script.js
@@ -7,33 +7,29 @@ $(document).ready(function()
     var streamUrl = 'https://api.twitch.tv/kraken/streams/';
     var channelUrl = 'https://api.twitch.tv/kraken/channels/';
     var $statusList = $('#status-list');
-    var listTemplate = '<li><a href="$URL"><img src="$IMG" class="img-thumbnail img-circle"/> $DISPLAY_NAME</a></li>';
+    var listTemplate = '<li><a href="$URL"><img src="$IMG" class="img-thumbnail img-circle"/> $DISPLAY_NAME' +
+        '<span class="glyphicon glyphicon-$STATUS">$PLAYING</span></a>' +
+        '</li>';
     var currentListItem;
     $statusList.empty();
 
     for (var i = 0; i < twitchUsers.length; i++)
     {
-        $.getJSON(channelUrl + twitchUsers[i], function (resp)
+        $.when($.getJSON(channelUrl + twitchUsers[i]), $.getJSON(streamUrl + twitchUsers[i])).done(function (user, stream)
         {
-             resp.logo = resp.logo === null? 'http://lorempixel.com/60/60/' : resp.logo;
-            currentListItem = listTemplate.replace('$URL', resp.url).replace('$IMG', resp.logo).replace('$DISPLAY_NAME', resp.display_name);
+            console.log(user[0].name);
+            console.log(stream[0]);
+            user[0].logo = user[0].logo === null ? 'http://lorempixel.com/60/60/' : user[0].logo;
+            currentListItem = listTemplate.replace('$URL', user[0].url).replace('$IMG', user[0].logo).replace('$DISPLAY_NAME', user[0].display_name);
+            if (stream[0].stream === null)
+            {
+                currentListItem = currentListItem.replace('$STATUS', 'pause').replace('$PLAYING', 'Offline');
+            }
+            else
+            {
+                currentListItem = currentListItem.replace('$STATUS', 'play').replace('$PLAYING', stream[0].stream.game);
+            }
             $statusList.append(currentListItem);
-        })
+        });
     }
-
-//for (i = 0; i < twitchUsers.length; i++)
-//{
-//    $.getJSON(streamUrl + twitchUsers[i], function(resp)
-//    {
-//        if (resp.stream === null)
-//        {
-//            console.log(resp._links.channel + ' is offline');
-//        }
-//        else
-//        {
-//            console.log('Playing ' + resp.stream.game);
-//            console.log(resp.stream.channel.name + " is online");
-//        }
-//    })
-//}
 });
