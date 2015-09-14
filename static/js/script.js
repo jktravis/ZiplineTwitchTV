ec650990ecb40a4293a800c0747c4723e5239965
jktravis <joshua.travis@gmail.com>
2015-09-14 13:44:53 -0400

    Filters have been added.

--- static/js/script.js
+++ static/js/script.js
@@ -10,34 +10,80 @@ $(document).ready(function()
     var listTemplate = '<li> <a href="$URL"><img src="$IMG" class="img-thumbnail img-circle"/> <span class="diplay-name">' +
         '$DISPLAY_NAME</span> <span class="status-icon glyphicon glyphicon-$STATUS"></span> <span class="activity">$PLAYING</span> </a> </li>';
     var currentListItem;
+    var $navli = $('.nav > li');
 
-    for (var i = 0; i < twitchUsers.length; i++)
+
+    $navli.click(function(e)
     {
-        $.when($.getJSON(channelUrl + twitchUsers[i]), $.getJSON(streamUrl + twitchUsers[i])).done(function (user, stream)
+        e.preventDefault();
+        $navli.removeClass('active');
+        $(this).addClass('active');
+
+        populateTwitchList(twitchUsers, $(this).find('a').text());
+    });
+
+    $('#filter').on('keyup', function()
+    {
+        var $this = $(this);
+        var filteredArray = twitchUsers.filter(function(val, index, arr)
         {
-            console.log(user[0].name);
-            console.log(stream[0]);
-            user[0].logo = user[0].logo === null ? 'http://lorempixel.com/60/60/' : user[0].logo;
-            currentListItem = listTemplate.replace('$URL', user[0].url).replace('$IMG', user[0].logo)
-                .replace('$DISPLAY_NAME', user[0].display_name);
-            if (stream[0].stream === null)
-            {
-                currentListItem = currentListItem.replace('$STATUS', 'pause').replace('$PLAYING', 'Offline');
-            }
-            else
-            {
-                currentListItem = currentListItem.replace('$STATUS', 'play').replace('$PLAYING', stream[0].stream.game);
-            }
-            $statusList.append(currentListItem);
-        }).fail(function()
-        {
-            $statusList.append('<li>Unable to load data</li>');
+            console.log('val.toLowerCase(): ' + val.toLowerCase());
+            console.log('$this.val().toLowerCase: ' + $this.val().toLowerCase());
+            return (val.toLowerCase().indexOf($this.val().toLowerCase()) > -1);
         });
+
+        console.log(filteredArray);
+
+        populateTwitchList(filteredArray, $('.active').find('a').text());
+    });
+
+    function populateTwitchList(arr, listType)
+    {
+        $statusList.empty();
+
+        for (var i = 0; i < arr.length; i++)
+        {
+            $.when($.getJSON(channelUrl + arr[i]), $.getJSON(streamUrl + arr[i])).done(function (user, stream)
+            {
+                user[0].logo = user[0].logo === null ? 'http://lorempixel.com/60/60/' : user[0].logo;
+                currentListItem = listTemplate.replace('$URL', user[0].url).replace('$IMG', user[0].logo)
+                    .replace('$DISPLAY_NAME', user[0].display_name);
+
+                if (stream[0].stream === null)
+                {
+                    currentListItem = currentListItem.replace('$STATUS', 'pause').replace('$PLAYING', 'Offline');
+                }
+                else
+                {
+                    currentListItem = currentListItem.replace('$STATUS', 'play').replace('$PLAYING', stream[0].stream.game);
+                }
+
+                if (listType === 'Online')
+                {
+                    if (stream[0].stream !== null)
+                    {
+                        $statusList.append(currentListItem);
+                    }
+                }
+                else if (listType === 'Offline')
+                {
+                    if (stream[0].stream === null)
+                    {
+                        $statusList.append(currentListItem);
+                    }
+                }
+                else
+                {
+                    $statusList.append(currentListItem);
+                }
+
+
+            }).fail(function()
+            {
+                $statusList.append('<li>Unable to load data</li>');
+            });
+        }
     }
 
-    $('.nav > li').click(function()
-    {
-        $('.nav > li').removeClass('active');
-        $(this).addClass('active');
-    })
+    populateTwitchList(twitchUsers, $('.active').find('a').text());
 });
